3d5790e1a7df65eb05b7cfa50af991fdc98b5f6b
Artem <artemkunakov70@gmail.com>
2026-03-27 00:05:09 +0300

    Commit: updated Readme.md

--- file_manager_lab/config.py
+++ file_manager_lab/config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 # Рабочая директория файлового менеджера.
 # При необходимости измените путь на свой.
 WORKSPACE_ROOT = Path(__file__).resolve().parent / 'workspace'
+MAX_SIZE_MB = 50
